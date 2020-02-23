55b0a6d1a39269d2f1ff6777c153054e981f1cdc
malijani <alijanialijanvandmohammad@gmail.com>
2020-02-23 10:17:24 +0330

    Deployed b0357f0 with updater

--- updater.sh
+++ updater.sh
@@ -2,6 +2,13 @@
 
 # script to auto update gh-pages branch of linux-essentials
 
+# check if branch is valid and script running in gh-pages
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" != "gh-pages" ]]; then
+  echo 'Aborting script'
+  exit 1
+fi
+
 # remove all files except these:
 shopt -s extglob
 rm -rv !(".gitignore"|"Sybex-LinuxEssentials2ED.pdf"|"CNAME"|".git"|".nojekyll"|".venv"|"updater.sh")
@@ -17,6 +24,7 @@ rm -rfv site/ site.zip
 # add files to git
 git add .
 # commit site update
-git commit -m "Updated site"
+COMMIT=$(git rev-parse --short HEAD)
+git commit -m "Deployed ${COMMIT} with updater"
 # push changes
 git push -u origin gh-pages
