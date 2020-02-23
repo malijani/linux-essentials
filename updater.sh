a5eb87bdcff08b9352aa037ea4226bbfd6912859
malijani <alijanialijanvandmohammad@gmail.com>
2020-02-23 09:49:16 +0330

    Added script to auto update gh-pages branch without harming files

---
+++ updater.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# script to auto update gh-pages branch of linux-essentials
+
+# remove all files except these:
+shopt -s extglob
+rm -rv !(".gitignore"|"Sybex-LinuxEssentials2ED.pdf"|"CNAME"|".git"|".nojekyll"|".venv"|"updater.sh")
+shopt -u extglob
+# copy site.zip from master branch
+git checkout master -- site.zip
+# unzip build of site
+unzip site.zip
+# move files from site directory
+mv site/* .
+# remove unneeded files
+rm -rfv site/ site.zip
